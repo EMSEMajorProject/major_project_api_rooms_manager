ffa312641f35114dc885b29f30df35fdc922e65d
CorentinDoue <doue.coco@hotmail.fr>
2017-11-14 11:51:26 +0100

    feat(switch-light) ok

--- src/main/java/fr/emse/majeureinfo/springbootintro/web/RoomController.java
+++ src/main/java/fr/emse/majeureinfo/springbootintro/web/RoomController.java
@@ -2,15 +2,17 @@ package fr.emse.majeureinfo.springbootintro.web;
 
 
 import fr.emse.majeureinfo.springbootintro.dao.RoomDao;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import fr.emse.majeureinfo.springbootintro.model.Light;
+import fr.emse.majeureinfo.springbootintro.model.Room;
+import fr.emse.majeureinfo.springbootintro.model.Status;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static fr.emse.majeureinfo.springbootintro.model.Status.OFF;
+import static fr.emse.majeureinfo.springbootintro.model.Status.ON;
 
 @RestController
 @RequestMapping(value = "/api/rooms")
@@ -18,6 +20,7 @@ import java.util.stream.Collectors;
 public class RoomController {
 
     private final RoomDao roomDao;
+    //private final LightDao lightDao;
 
 
     public RoomController(RoomDao roomDao) {
@@ -29,5 +32,14 @@ public class RoomController {
         return roomDao.findAll().stream().map(RoomDto::new).collect(Collectors.toList());
     }
 
+    @PostMapping(value = "/{id}/switch-light")
+    public LightDto switchLight(@PathVariable("id") Long id){
+        Room room = roomDao.getOne(id);
+        Light light = room.getLight();
+        Status lightstatus = light.getStatus();
+        light.setStatus(lightstatus == ON ? OFF : ON);
+        LightDto lightDto = new LightDto(light);
+        return lightDto;
+    }
 }
 
