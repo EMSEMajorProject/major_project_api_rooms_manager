178b6804d4f89016c8fe0d651efa5f50344491ab
CorentinDoue <doue.coco@hotmail.fr>
2017-11-14 10:16:04 +0100

    remove hello

--- src/main/java/fr/emse/majeureinfo/springbootintro/SpringBootIntroApplication.java
+++ src/main/java/fr/emse/majeureinfo/springbootintro/SpringBootIntroApplication.java
@@ -1,13 +1,10 @@
 package fr.emse.majeureinfo.springbootintro;
 
 
-import fr.emse.majeureinfo.springbootintro.hello.ConsoleGreetingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication public class SpringBootIntroApplication {
-
-	private ConsoleGreetingService consoleGreetingService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootIntroApplication.class, args);
--- src/main/java/fr/emse/majeureinfo/springbootintro/hello/GreetingService.java
+++
@@ -1,9 +0,0 @@
-package fr.emse.majeureinfo.springbootintro.hello;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface GreetingService {
-
-    void greet(String name);
-}
